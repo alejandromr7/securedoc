740d351658ae1b2bdf14003b39294f8513f2ab2a
Alejandro <alejandro_mr7@outlook.com>
2025-01-17 22:29:59 -0600

    Entity Listener - Part 2

--- src/main/java/securedoc/entity/Auditable.java
+++ src/main/java/securedoc/entity/Auditable.java
@@ -2,10 +2,11 @@ package securedoc.entity;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.util.AlternativeJdkIdGenerator;
 
@@ -17,11 +18,26 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 @JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
 public abstract class Auditable {
+    @Id
+    @SequenceGenerator(name = "primary_key_seq", sequenceName = "primary_key_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "primary_key_seq")
+    @Column(name = "id", updatable = false)
+
     private Long id;
     private String referencedId = new AlternativeJdkIdGenerator().generateId().toString();
+
+    @NotNull
     private Long createdBy;
     private Long updatedBy;
+
+    @NotNull
+    @CreatedDate
+    @Column(name = "updated_at", updatable = false, nullable = false)
     private LocalDateTime updatedAt;
+
+    @NotNull
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
     private LocalDateTime createdAt;
 
 }
